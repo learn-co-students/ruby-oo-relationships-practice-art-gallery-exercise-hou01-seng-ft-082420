99cc7b52c913aa5cc143250e8a5d45bed73289f2
IainLR <iainrobertzon@gmail.com>
2020-08-29 17:37:47 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,49 @@
+#require 'pry'
 class Artist
-
+  @@all = []
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    self.class.all << self 
   end
+
+  def self.all
+    @@all
+  end 
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end 
+
+  def galleries
+    paintings.map {|art| art.gallery}
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}.uniq
+  end
+
+
+  def self.total_experience
+    @@all.map {|artist| artist.years_experience}.sum
+  end 
+
+  # def painting_quantity
+  #   paintings.count
+  #   #self.map {|artist| artist.paintings.count.to_f / artist.years_experience.to_f}
+  # end 
+
+  def self.most_prolific
+    #@@all.map {|artist| artist.painting_quantity.to_f / artist.years_experience.to_f}
+    @@all.max_by {|artist| artist.paintings.count.to_f / artist.years_experience.to_f }
+
+  end 
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end 
+  #binding.pry 
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,36 @@
 class Gallery
-
+  @@all = []
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    self.class.all << self
+  end
+
+  def self.all 
+    @@all 
+  end 
+
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    paintings.map {|painting| painting.artist}
+  end 
+
+  def artist_names
+    artists.map {|artist| artist.name}.uniq
+  end
+
+  def most_expensive_painting
+    #paintings
+    expensive_paint = paintings.max do |paint_a, paint_b|
+      paint_a.price <=> paint_b.price
+    end
+   
+    #expensive_paint.title #this returns only the name
   end
 
 end
