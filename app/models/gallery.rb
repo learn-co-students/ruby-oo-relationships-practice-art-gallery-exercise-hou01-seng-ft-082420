31196826a7c1f130fa30a638ccae2945913e4b8e
mlarcher531 <mlarcher531@gmail.com>
2020-09-02 09:56:15 -0500

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,34 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    self.class.all.push(self)
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    self.paintings.collect {|painting| painting.artist}
+  end
+
+  def artist_names
+    self.artists.collect {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    self.paintings.max {|painting| painting.price}
+  end
+  
+    
 
 end
